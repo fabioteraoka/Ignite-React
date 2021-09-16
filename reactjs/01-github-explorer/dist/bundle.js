fca6a085610b4beabcdd49b95174cabbd09c7068
Fabio Teraoka <65736019+fabioteraoka@users.noreply.github.com>
2021-09-16 06:39:45 -0300

    Initial commit

---
+++ reactjs/01-github-explorer/dist/bundle.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var _react = _interopRequireDefault(require("react"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+function App() {
+  return /*#__PURE__*/_react["default"].createElement("div", {
+    className: "App"
+  }, /*#__PURE__*/_react["default"].createElement("h1", null, "Hello React"));
+}
