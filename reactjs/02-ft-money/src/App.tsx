04cb941cb1f820d5c1cdcbf457f434addf65ac39
Fabio Teraoka <65736019+fabioteraoka@users.noreply.github.com>
2021-09-22 21:12:46 -0300

    Configuration

---
+++ reactjs/02-ft-money/src/App.tsx
@@ -0,0 +1,14 @@
+import { Header } from "./components/Header";
+import { GlobalStyle } from "./styles/global";
+
+
+export function App() {
+  return (
+    <div className="App">
+        <Header />
+        <h1>Teraoka</h1>
+        <GlobalStyle/>
+    </div>
+  );
+}
+
---
+++ reactjs/02-ft-money/src/components/Header/index.tsx
@@ -0,0 +1,9 @@
+
+
+export const Header = () => {
+    return (
+        <div>
+            <h1>Fabio</h1>
+        </div>
+    )
+}
