46aa60525da460d304274e261b3cfb9098a6f6a5
Fabio Teraoka <65736019+fabioteraoka@users.noreply.github.com>
2021-10-14 21:23:22 -0300

    Static Site Generation (SSG)

--- reactjs/03-ignews/src/pages/index.tsx
+++ reactjs/03-ignews/src/pages/index.tsx
@@ -1,8 +1,12 @@
-import { GetServerSideProps } from "next";
+import { GetStaticProps } from "next";
 import Head from "next/head";
 import { SubscribeButton } from "../components/SubscribeButton";
 import { stripe } from "../services/stripe";
 import styles from './home.module.scss'
+
+// Client-side
+// Server-side
+// Static-site Generation
 
 interface HomeProps{
   product: {
@@ -35,7 +39,7 @@ export default function Home({product}: HomeProps) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
     const price = await stripe.prices.retrieve('price_1JkHRuG5FnLAdke7Bq1WFyZZ')
     const product = {
       priceId: price.id,
@@ -47,6 +51,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
     return {
       props: {
         product
-      }
+      },
+      revalidate: 60 * 60 * 24, // 1 day
     }
   }
