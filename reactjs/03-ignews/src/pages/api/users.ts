3b2d936c95537b8e354709bb0567e818e8d6405d
Fabio Teraoka <65736019+fabioteraoka@users.noreply.github.com>
2021-10-15 21:32:17 -0300

    API routes no Next.JS

--- reactjs/03-ignews/src/pages/api/users.ts
+++ reactjs/03-ignews/src/pages/api/users.ts
@@ -3,9 +3,9 @@ import { NextApiRequest, NextApiResponse } from "next";
 export default (request:NextApiRequest, response:NextApiResponse) =>{
     const users = [
         { id:1, name: "Fabio"},
-        { id:1, name: "Silvana"},
-        { id:1, name: "Carolina"},
-        { id:1, name: "Maggie"},
+        { id:2, name: "Silvana"},
+        { id:3, name: "Carolina"},
+        { id:4, name: "Maggie"},
     ]
 
     return response.json(users)
