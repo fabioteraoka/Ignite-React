f952500c0c3beeb4aedc9cfad04f09071dcedbf2
Fabio Teraoka <65736019+fabioteraoka@users.noreply.github.com>
2021-10-16 19:14:52 -0300

    Verificando usuario duplicado

--- reactjs/03-ignews/src/pages/api/auth/[...nextauth].ts
+++ reactjs/03-ignews/src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,9 @@
+import { query as q} from "faunadb"
+
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+
+import { fauna } from "../../../services/fauna"
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -9,6 +13,39 @@ export default NextAuth({
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
       scope: 'read:user'
     }),
-    // ...add more providers here
   ],
+  callbacks: {
+    async signIn( user, account, profile ) {
+
+      const { email } = user
+      try{
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index("index_by_email"),
+                  q.Casefold(user.email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'), 
+              { data: { email: email }}
+            ),
+            q.Get(
+              q.Match(
+                q.Index("index_by_email"),
+                q.Casefold(user.email)
+              )
+            )
+          )
+        )
+        return true
+      } catch {
+        return false
+      }
+      
+    }
+  }
 })
