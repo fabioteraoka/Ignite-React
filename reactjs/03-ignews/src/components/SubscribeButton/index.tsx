faefadbabdb7c2dfb767bdddbae16bfa51877d89
Fabio Teraoka <65736019+fabioteraoka@users.noreply.github.com>
2021-10-17 17:26:34 -0300

    Gerando sessão de checkout

--- reactjs/03-ignews/src/components/SubscribeButton/index.tsx
+++ reactjs/03-ignews/src/components/SubscribeButton/index.tsx
@@ -1,3 +1,4 @@
+import { signIn, useSession } from 'next-auth/client';
 import styles from './styles.module.scss'
 
 interface SubscribeButtonProps {
@@ -6,10 +7,19 @@ interface SubscribeButtonProps {
 
 
 export function SubscribeButton({priceId}: SubscribeButtonProps) {
+    const [session] = useSession();
+
+    function handleClick() {
+        if (!session) {
+            signIn('github');
+            return;
+        }
+    }
     return (
         <button 
         type="button"
         className={styles.subscribeButton}
+        onClick={handleClick}
         >
             Subscribe now
         </button>
