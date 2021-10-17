933a8dedd033228b659130ac9c0740538b11fa97
Fabio Teraoka <65736019+fabioteraoka@users.noreply.github.com>
2021-10-17 18:20:08 -0300

    Redirecionando para o Stripe

--- reactjs/03-ignews/src/components/SubscribeButton/index.tsx
+++ reactjs/03-ignews/src/components/SubscribeButton/index.tsx
@@ -1,4 +1,6 @@
 import { signIn, useSession } from 'next-auth/client';
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss'
 
 interface SubscribeButtonProps {
@@ -9,12 +11,26 @@ interface SubscribeButtonProps {
 export function SubscribeButton({priceId}: SubscribeButtonProps) {
     const [session] = useSession();
 
-    function handleClick() {
+    async function handleClick() {
         if (!session) {
             signIn('github');
             return;
         }
+
+        try { 
+        const response = await api.post('/subscribe')
+
+        const { sessionId } = response.data;
+
+        const stripe = await getStripeJs();
+
+        await stripe.redirectToCheckout({sessionId});
+
+        } catch (error) {
+            alert(error.message);
+        }  
     }
+
     return (
         <button 
         type="button"
--- reactjs/03-ignews/src/pages/api/subscribe.ts
+++ reactjs/03-ignews/src/pages/api/subscribe.ts
@@ -12,7 +12,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
         })
 
         const stripeCheckoutSession = await stripe.checkout.sessions.create({
-            customer: stripeCustomer.id, 
+            customer: (await stripeCustomer).id, 
             payment_method_types: ['card'],
             billing_address_collection: 'required',
             line_items: [{
