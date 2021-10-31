08b0cd55dddf5ccc417370833d5fd9839c1f8d6a
Fabio Teraoka <65736019+fabioteraoka@users.noreply.github.com>
2021-10-31 10:38:01 -0300

    Pagina: Posts

---
+++ reactjs/03-ignews/src/pages/posts/index.tsx
@@ -0,0 +1,33 @@
+import Head  from 'next/head'
+import styles from './styles.module.scss'
+
+
+export default function Posts () {
+  return (
+    <>
+      <Head>
+        <title>Posts | Ignews</title> 
+      </Head>
+
+      <main className={styles.container}>
+          <div className={styles.posts}>
+              <a href="#">
+                  <time>31 de outubro de 2021</time>
+                  <strong>125 MELHORES SITES PORNO DE 2021</strong>
+                  <p> internet revolucionou a nossa forma de consumir pornografia, e os sites pornô vieram pra ficar. Antes de tudo, desde 2012 o Testosterona criou e atualiza esta lista com 15 melhores sites porno, que depois passou pra 50, 100 e hoje estamos com nada menos do que 125 sites de pornografia!! Confira os melhores sites pornos e sites de sexo da atualidade. Qual melhor site porno? Aqui você vai encontrar os melhores sites pornográficos. Tem porno pra todos os gostos.</p>
+              </a>
+              <a href="#">
+                  <time>31 de outubro de 2021</time>
+                  <strong>125 MELHORES SITES PORNO DE 2021</strong>
+                  <p> internet revolucionou a nossa forma de consumir pornografia, e os sites pornô vieram pra ficar. Antes de tudo, desde 2012 o Testosterona criou e atualiza esta lista com 15 melhores sites porno, que depois passou pra 50, 100 e hoje estamos com nada menos do que 125 sites de pornografia!! Confira os melhores sites pornos e sites de sexo da atualidade. Qual melhor site porno? Aqui você vai encontrar os melhores sites pornográficos. Tem porno pra todos os gostos.</p>
+              </a>
+              <a href="#">
+                  <time>31 de outubro de 2021</time>
+                  <strong>125 MELHORES SITES PORNO DE 2021</strong>
+                  <p> internet revolucionou a nossa forma de consumir pornografia, e os sites pornô vieram pra ficar. Antes de tudo, desde 2012 o Testosterona criou e atualiza esta lista com 15 melhores sites porno, que depois passou pra 50, 100 e hoje estamos com nada menos do que 125 sites de pornografia!! Confira os melhores sites pornos e sites de sexo da atualidade. Qual melhor site porno? Aqui você vai encontrar os melhores sites pornográficos. Tem porno pra todos os gostos.</p>
+              </a>
+          </div>
+      </main>
+    </>
+  )
+}
