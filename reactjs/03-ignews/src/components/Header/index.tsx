5ee2fa8c94810e04568787a2b760768c7e34c272
Fabio Teraoka <65736019+fabioteraoka@users.noreply.github.com>
2021-10-31 14:24:52 -0300

    Component:e: ActiveLink

--- reactjs/03-ignews/src/components/Header/index.tsx
+++ reactjs/03-ignews/src/components/Header/index.tsx
@@ -1,19 +1,20 @@
-import Link from 'next/link'
 import styles from "./styles.module.scss";
 import {SignInButton} from "../SignInButton";
+import { ActiveLink } from "../ActiveLink";
 
 export function Header() {
+  
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="ig.news" />
         <nav>
-          <Link href="/">         
-          <a className={styles.active}>Home</a>
-          </Link>
-          <Link href="/posts">
+          <ActiveLink activeClassName={styles.active} href="/">         
+          <a>Home</a>
+          </ActiveLink>
+          <ActiveLink activeClassName={styles.active} href="/posts">
           <a>Post</a>
-          </Link>
+          </ActiveLink>
         </nav>
 
         <SignInButton/>
