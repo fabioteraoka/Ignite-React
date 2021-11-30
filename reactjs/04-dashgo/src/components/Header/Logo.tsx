740c92c885743ea26c052658d5ec4421bd200b1d
Fabio Teraoka <65736019+fabioteraoka@users.noreply.github.com>
2021-11-29 21:09:12 -0300

    Responsividade no Chakra

--- reactjs/04-dashgo/src/components/Header/Logo.tsx
+++ reactjs/04-dashgo/src/components/Header/Logo.tsx
@@ -3,7 +3,7 @@ import { Text } from "@chakra-ui/react";
 
 export function Logo() {
   return (
-    <Text fontSize="3x1" fontWeight="bold" letterSpacing="tight" w="64">
+    <Text fontSize={["2xl","3x1"]} fontWeight="bold" letterSpacing="tight" w="64">
       dashgo
       <Text as="span" ml="1" color="pink.500">
         .
