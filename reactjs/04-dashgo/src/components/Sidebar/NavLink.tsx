6156e3b5ea7103d9ee8c4cf81601047c149e256e
Fabio Teraoka <65736019+fabioteraoka@users.noreply.github.com>
2021-12-01 20:46:03 -0300

    Sinalizando link ativo

--- reactjs/04-dashgo/src/components/Sidebar/NavLink.tsx
+++ reactjs/04-dashgo/src/components/Sidebar/NavLink.tsx
@@ -5,7 +5,7 @@ import {
   LinkProps as ChakraLinkProps,
 } from "@chakra-ui/react";
 import { ElementType } from "react";
-import Link from "next/link";
+import { ActiveLink } from "../ActiveLink";
 
 interface NavLinkProps extends ChakraLinkProps {
   icon: ElementType;
@@ -15,13 +15,13 @@ interface NavLinkProps extends ChakraLinkProps {
 
 export default function NavLink({ icon, children, href,  ...rest }: NavLinkProps) {
   return (
-    <Link href={href} passHref>
+    <ActiveLink href={href} passHref>
       <ChakraLink display="flex" align="stretch" {...rest}>
         <Icon as={icon} fontSize="20" />
         <Text m="0" ml="4" fontWeight="medium" align="center">
           {children}
         </Text>
       </ChakraLink>
-    </Link>
+    </ActiveLink>
   );
 }
