4316ae8df1c064656f03e1fffb12e9f64ece77b6
Fabio Teraoka <65736019+fabioteraoka@users.noreply.github.com>
2021-12-01 20:13:46 -0300

    Cadastro responsivo

--- reactjs/04-dashgo/src/pages/users/create.tsx
+++ reactjs/04-dashgo/src/pages/users/create.tsx
@@ -9,11 +9,11 @@ export default function CreateUser() {
             <Header/>
             <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
                 <Sidebar/>
-                <Box flex="1" borderRadius={8} bg="gray.800" p="8">
+                <Box flex="1" borderRadius={8} bg="gray.800" p={["6","8"]}>
                     <Heading size="lg" fontWeight="normal">Criar usuário</Heading>
                     <Divider my="6" borderColor="gray.700"/>
                     <VStack spacing="8">
-                        <SimpleGrid minChildWidth="240px" spacing="8" w="100%" >
+                        <SimpleGrid minChildWidth="240px" spacing={["6","8"]} w="100%" >
                             <Box>
                                 <Text mb="8px">Nome completo</Text>
                                 <Input name="name" label="nome completo" color="white"/>
@@ -23,7 +23,7 @@ export default function CreateUser() {
                             <Input name="email" type="email" label="E-mail" color="white"/>
                             </Box>
                         </SimpleGrid>
-                        <SimpleGrid minChildWidth="240px" spacing="8" w="100%" >
+                        <SimpleGrid minChildWidth="240px" spacing={["6","8"]} w="100%" >
                             <Box>
                                 <Text mb="8px">Senha</Text>
                                 <Input name="password" type= "password" label="senha" color="white"/>
