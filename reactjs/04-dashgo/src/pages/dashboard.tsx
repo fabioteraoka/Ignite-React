2e6b9dc45b9bd12b1a749445ede332a7cbcca35c
Fabio Teraoka <65736019+fabioteraoka@users.noreply.github.com>
2021-12-01 19:57:29 -0300

    Dashboard responsivo

--- reactjs/04-dashgo/src/pages/dashboard.tsx
+++ reactjs/04-dashgo/src/pages/dashboard.tsx
@@ -65,7 +65,7 @@ export default function dashboard() {
       <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
             <Sidebar/> 
             <SimpleGrid flex="1" gap="4" minChildWidth="320px" align="flex-start">
-            <Box p="8"
+            <Box p={["6","8"]}
                 bg="gray.800"
                 borderRadius={8}
                 pb="4"
@@ -73,7 +73,7 @@ export default function dashboard() {
                     <Text fontSize="lg" mb="4">Inscrito da semana</Text>
                     <Chart options={options} series={series} type="area" height={160}/>
             </Box>
-            <Box p="8"
+            <Box p={["6","8"]}
                 bg="gray.800"
                 borderRadius={8}
                 pb="4"
