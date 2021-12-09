78645a0b5526753f0f2fd5f8b6bf957eade32cb6
Fabio Teraoka <65736019+fabioteraoka@users.noreply.github.com>
2021-12-09 19:06:42 -0300

    Utilizando mutations

--- reactjs/04-dashgo/src/services/mirage/index.ts
+++ reactjs/04-dashgo/src/services/mirage/index.ts
@@ -1,4 +1,4 @@
-import { createServer, Factory, Model, Response} from 'miragejs'
+import { createServer, Factory, Model, Response, ActiveModelSerializer} from 'miragejs'
 import faker from 'faker'
 
 type User={
@@ -9,6 +9,10 @@ type User={
 
 export function makeServer(){
     const server = createServer({
+        serializers: {
+            application: ActiveModelSerializer,
+        },
+
         models: {
             user: Model.extend<Partial<User>>({})
         },
@@ -29,9 +33,10 @@ export function makeServer(){
 
 
         seeds(server){
-            server.createList('user', 200);
+            server.createList('user', 29);
         },
         routes() {
+            
             this.namespace ="api";
             this.timing = 750;
 
